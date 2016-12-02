5d9282ea804ba0ea325f5fd37fc4375ab24259d5
christianJCK <jecko.christian@gmail.com>
2016-12-02 22:43:03 +0100

    controller setup

---
+++ src/AppBundle/Controller/InspireController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apprenti
+ * Date: 02/12/16
+ * Time: 22:40
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class InspireController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+}
